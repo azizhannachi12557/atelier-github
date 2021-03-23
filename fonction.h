d3c718d7d2f5fe053b185498bfbac098a8d78fef
azizos <aziz.hannachi@esprit.tn>
2021-03-23 05:16:38 -0700

    MAJ fonction

---
+++ fonction.h
@@ -0,0 +1,11 @@
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+#include <SDL/SDL.h>
+
+
+int scroll1(SDL_Surface *ecran,SDL_Surface *imageDeFond ,int *continuer,SDL_Rect positionFond,int *save,int level) ;
+int scroll2(SDL_Surface *ecran,SDL_Surface *imageDeFond ,int *continuer,SDL_Rect positionFond,int *save,int level) ;
+int scroll3(SDL_Surface *ecran,SDL_Surface *imageDeFond ,int *continuer,SDL_Rect positionFond,int *save,int level) ;
+
+
+#endif // JEU_H_INCLUDED
